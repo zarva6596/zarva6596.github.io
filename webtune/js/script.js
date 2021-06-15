685b68f7bcd119bc61a063c1e0536120e1e05d80
Roman Zarvanskyi <67337360+zarva6596@users.noreply.github.com>
2021-06-15 15:09:45 +0300

    fixed 4.5

--- webtune/js/script.js
+++ webtune/js/script.js
@@ -494,6 +494,13 @@ $(window).on('load', function() {
       'visibility': 'visible',
       'opacity': '1',
    });
-
-   window.scrollTo(0, 1);
 });
+
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+   /* iOS hides Safari address bar */
+   window.addEventListener("load",function() {
+       setTimeout(function() {
+           window.scrollTo(0, 1);
+       }, 1000);
+   });
+}
