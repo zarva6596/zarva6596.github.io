dd96692afe3548178167c19b01a5c18382ee99ae
Roman Zarvanskyi <67337360+zarva6596@users.noreply.github.com>
2021-06-24 15:07:15 +0300

    created mobile version

--- optic/js/script.js
+++ optic/js/script.js
@@ -1,25 +1,3 @@
-// $('.products-slider__slider').slick({
-//    slidesToShow: 4,
-//    slidesToScroll: 1,
-//    dots: false,
-//    responsive: [
-//       {
-//         breakpoint: 1219,
-//         settings: {
-//           slidesToShow: 3,
-//           slidesToScroll: 1,
-//         }
-//       },
-//       {
-//          breakpoint: 767,
-//          settings: {
-//            slidesToShow: 2,
-//            slidesToScroll: 1,
-//          }
-//        },
-//    ],
-// });
-
 $('.main-slider__slider').slick({
    arrows: false,
    dots: true, 
@@ -27,25 +5,54 @@ $('.main-slider__slider').slick({
 });
 
 $('.services__textSlider').slick({
-   asNavFor: '.services__boxSlider'
+   asNavFor: '.services__boxSlider',
+   responsive: [
+      {
+         breakpoint: 769,
+         settings: {
+            arrows: false,
+         }
+      }
+   ],
 });
 
 $('.services__boxSlider').slick({
    arrows: false,
    variableWidth: true,
    asNavFor: '.services__textSlider',
+   responsive: [
+      {
+         breakpoint: 769,
+         settings: {
+            arrows: true,
+            dots: true,
+         }
+      }
+   ],
 });
 
 $('.products__slider').slick({
    variableWidth: true,
+   responsive: [
+      {
+         breakpoint: 769,
+         settings: {
+            dots: true,
+         }
+      }
+   ],
 });
 
 let productsTabs = $('.products__menu > .products__item');
 let whereTabs = $('.where__menu > .where__item');
 
-$(window).on('load', function () {
+$(window).on('load resize', function () {
    $(productsTabs[2]).addClass('products__item--active');
    $(whereTabs[1]).addClass('where__item--active');
+
+   if ($(window).width() < 1161) {
+      $('.footer__column > p').after($('.footer__contacts'));
+   }
 });
 
 $(productsTabs).on('click', function() {
